9ce1144bc1c0e71e23fb6d87ab2952422f7d81d0
Superfelicia <felicia.ahlstrom@hotmail.se>
2024-02-14 10:28:44 +0000

    initial commit

---
+++ forms-laravel/app/Http/Controllers/PostController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('add-blog-post-form');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $post = new Post;
+        $post->title = $request->title;
+        $post->description = $request->description;
+        $post->save();
+        // session till vy genom att skriva 'with'
+        return redirect('add-blog-post-form')->with('status', 'Blog Post Form Data Has Been Inserted');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+
+     // Vi kan använda sessions i $request objektet i en controller:
+     // public function show(Request $request, $id) 
+     //{
+        //$value = $request->session()->get('key);
+     //}
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ forms-laravel/resources/views/add-blog-post-form.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel 8 Form Example Tutorial</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    @vite('resources/css/app.css')
+</head>
+<body>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+</body>
+</html>
